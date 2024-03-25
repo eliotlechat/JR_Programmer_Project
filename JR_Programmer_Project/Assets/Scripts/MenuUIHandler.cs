c49207d126d70407b533e4e5fd3a90e78bc45ba3
MaryPerso <marykita.contact@gmail.com>
2024-03-25 08:19:12 +0100

    push PC perso

--- JR_Programmer_Project/Assets/Scripts/MenuUIHandler.cs
+++ JR_Programmer_Project/Assets/Scripts/MenuUIHandler.cs
@@ -29,6 +29,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -38,6 +39,7 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
 
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
@@ -54,5 +56,16 @@ public class MenuUIHandler : MonoBehaviour
         }
         */
 
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
